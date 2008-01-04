cc7263f19b63347bbb3ec55bbc1647defcd39b29
rob.see <rob.see@de5ee365-d638-0410-ac67-a579fd33fef4>
2008-01-04 04:12:13 +0000

    Fix for DMPUTFROM... should fix issue 9

--- trunk/server/var/www/dotmac/perlmodules/DotMac/DMTransHandler.pm
+++ trunk/server/var/www/dotmac/perlmodules/DotMac/DMTransHandler.pm
@@ -7,8 +7,9 @@ use Apache2::RequestIO ();
 use Apache2::RequestRec ();
 use Apache2::RequestUtil ();
 use Apache2::Log;
+use Apache2::URI ();
 
-use Apache2::Const -compile => qw(DECLINED);
+use Apache2::Const -compile => qw(:methods DECLINED);
 use APR::Const    -compile => qw(:error SUCCESS);
 use CGI::Carp;
 use DotMac::CommonCode;
@@ -25,11 +26,14 @@ sub handler
 			my $httpType="http://";
 			$httpType="https://" if $r->get_server_port() == 443;
 			$logging =~ m/Sections/&&$rlog->info($httpType.$r->headers_in->{'Host'}.$r->uri." ".$r->headers_in->{'X-Source-Href'});
-			$r->headers_in->{'Destination'}=$httpType.$r->headers_in->{'Host'}.$r->uri;
+			$r->headers_in->{'Destination'}=$r->construct_url($r->uri);
+			$logging =~ m/TransHandler/&&$r->log->info("New Source: ".DotMac::CommonCode::URLDecode($r->headers_in->{'X-Source-Href'})); 
+			$logging =~ m/TransHandler/&&$r->log->info("New Dest: ".$r->construct_url($r->uri)); 
 			$r->uri(DotMac::CommonCode::URLDecode($r->headers_in->{'X-Source-Href'}));
 			#$r->header_in->{'uri'}=$r->header_in->{'X-Source-Href'};
-			$r->method("MOVE");
-		#	$logging =~ m/Sections/&&$rlog->info($r->as_string());
+			$r->method('MOVE');
+			$r->method_number(Apache2::Const::M_MOVE);
+			$logging =~ m/Sections/&&$rlog->info($r->as_string());
 			}
 	#	elsif (($r->method() eq "POST") && ($r->headers_in->{'X-Webdav-Method'}) && ($r->headers_in->{'X-Webdav-Method'} eq "DMOVERLAY")){
 #			$logging =~ m/Sections/&&$rlog->info("In the DMOVERLAY to MOVE TransHandler");
