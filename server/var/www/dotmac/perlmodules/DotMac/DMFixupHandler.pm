18d168e2cb1a79b0ad48193b8a5515a2a5d011ec
rob.see <rob.see@de5ee365-d638-0410-ac67-a579fd33fef4>
2008-01-19 23:42:20 +0000

    Added license blurbs to files

--- server/var/www/dotmac/perlmodules/DotMac/DMFixupHandler.pm
+++ server/var/www/dotmac/perlmodules/DotMac/DMFixupHandler.pm
@@ -1,5 +1,23 @@
 #file:DotMac/DMFixupHandler.pm
 #--------------------------------
+
+## Copyright (C) 2007 Walinsky, Robert See
+## This file is part of dotMac. 
+
+## dotMac is free software: you can redistribute it and/or modify
+## it under the terms of the Affero GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+
+## dotMac is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## Affero GNU General Public License for more details.
+
+## You should have received a copy of the Affero GNU General Public License
+## along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+
 package DotMac::DMFixupHandler;
 
 use strict;
