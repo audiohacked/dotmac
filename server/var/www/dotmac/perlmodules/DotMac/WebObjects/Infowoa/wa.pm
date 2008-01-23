e9c9e5a4c98eb489e0f19490d2afae3ba46312ea
audiohacked <audiohacked@de5ee365-d638-0410-ac67-a579fd33fef4>
2008-01-23 06:31:59 +0000

    fixed issue 4 & 25 where special characters or dots in the screen name (4) or in the password (25) were rejected/not-allowed

--- server/var/www/dotmac/perlmodules/DotMac/WebObjects/Infowoa/wa.pm
+++ server/var/www/dotmac/perlmodules/DotMac/WebObjects/Infowoa/wa.pm
@@ -105,6 +105,9 @@ sub retrieveDiskConfiguration {
 		elsif ($name =~ m/password/){ $password = $value; }
 		elsif ($name =~ m/version/){ $dotmacversion = $value; }
 		}
+		$username =~ s/^\"|\"$//g;
+		$password =~ s/^\"|\"$//g;
+
 	#carp "username $username, version $dotmacversion";
 	# if (DotMac::CommonCode::authen_user($r, $username, $password))
 	my $home_dir = $r->dir_config('dotMaciDiskPath') . "/$username";
@@ -158,6 +161,8 @@ sub dotMacPreferencesPaneMessage {
 		elsif ($name =~ m/systemVersion/){ $systemVersion = $value; }
 		elsif ($name =~ m/version/){ $dotmacversion = $value; }
 		}
+		$username =~ s/^\"|\"$//g;
+		$password =~ s/^\"|\"$//g;
 
 	if (DotMac::CommonCode::authen_user($r, $username, $password))
 		{
