4603b686b94586706e89cbfb4b8648ba45ea09b8
audiohacked <audiohacked@de5ee365-d638-0410-ac67-a579fd33fef4>
2008-01-24 08:54:50 +0000

    [add] working mod_perl authen for apache; no need for mod_authn_dbd

--- branches/dbi-dotmac/server/var/www/dotmac/perlmodules/DotMac/AuthenDigestDM.pm
+++ branches/dbi-dotmac/server/var/www/dotmac/perlmodules/DotMac/AuthenDigestDM.pm
@@ -1,32 +1,42 @@
 package DotMac::AuthenDigestDM;
 
-use Apache::Log;
-use Apache::Const -compile => qw(OK DECLINED HTTP_UNAUTHORIZED);
-
+use Apache2::Const -compile => qw(OK DECLINED HTTP_UNAUTHORIZED);
+use CGI::Carp;
 use strict;
 
 sub handler {
 	my ($r, $user, $realm, $hash) = @_;
-	my $dbType = $r->dir_config('dotMacDBServType');
-	my $dbauth = '';
+	#my $dbType = $r->dir_config('dotMacDBServType');
+	#carp $dbType;
+	my $dbType = 'mysql';
+	my $dbauth;
+
+	#carp "AuthenDigestDM";
+	#carp $dbType;
+	#carp $user;
+	#carp $realm;
+	
 	if ($dbType eq 'mysql') { # mysql
 		use DotMac::DotMacDB::MySQL;
-		$dbauth = DotMac::DotMacDB::MySQL;
-	} elsif ($dbType eq 'sqlite') {
-		use DotMac::DotMacDB::SQLite;
-		$dbauth = DotMac::DotMacDB::SQLite;
+		$dbauth = new DotMac::DotMacDB::MySQL;
+		#carp "using mysql";
+	#} elsif ($dbType eq 'sqlite') {
+	#	use DotMac::DotMacDB::SQLite;
+	#	$dbauth = new DotMac::DotMacDB::SQLite;
+	#	carp "using sqlite";
 	} else {
-		return Apache::DECLINED;
+		return Apache2::Const::DECLINED;
 	}
 
+	#carp "exec fetch_hash";
 	my $savedHash = $dbauth->fetch_hash($user, $realm);
-
-	if ($hash eq $saveHash) {
-		return Apache::OK;
+	#carp $savedHash;
+	if($savedHash) {
+		$$hash = $savedHash;
+		return Apache2::Const::OK;
 	} else {
-		return Apache::HTTP_UNAUTHORIZED;
-		#return Apache::DECLINED;
+		return Apache2::Const::HTTP_UNAUTHORIZED;
+		#return Apache2::ConstDECLINED;
 	}
 }
-
 1;
--- branches/dbi-dotmac/server/var/www/dotmac/perlmodules/DotMac/DotMacDB/MySQL.pm
+++ branches/dbi-dotmac/server/var/www/dotmac/perlmodules/DotMac/DotMacDB/MySQL.pm
@@ -1,80 +1,163 @@
 package DotMac::DotMacDB::MySQL;
 
 use DBI;
+use strict;
+use CGI::Carp;
 
-my $dsn = "DBI:mysql:database=dotmac;host=localhost";
-my $dbuser = 'dotmac';
-my $dbpass = 'dotmac';
+#sub new {
+#	my ($class, @args) = @_;
+#
+#	eval "require $class" unless $class->can('new');
+#	my ($host, $db, $dbuser, $dbpass, $create) 
+#		= rearrange([
+#			[qw(HOST)],
+#			[qw(DB)],
+#			[qw(DBUSER USER)],
+#			[qw(DBPASS PASSWD)],
+#			'CREATE'
+#		],@args);
+#
+#	my $host ||= 'localhost';
+#	my $db ||= 'dotmac';
+#	my $dbuser ||= 'dotmac';
+#	my $dbpass ||= 'dotmac';
+#	#my $this = bless {}, $class;
+#
+#	my $dbcon = DBI->connect("dbi:mysql:$db".';host='.$host, $dbuser, $dbpass);
+#
+#	return bless { dbh => $dbcon }, $class;
+#}
+
+sub new {
+	my $invocant = shift;
+	my $class = ref($invocant) || $invocant;
+	my $dotmacDBconn = DBI->connect("dbi:mysql:dotmac;host=localhost", "dotmac", "dotmac");
+	my $self = {
+		dbh => $dotmacDBconn,
+		@_
+	};
+	return bless $self, $class;
+}
 
 sub fetch_hash{
+	my $self = shift;
 	my ($user, $realm) = @_;
-	my $dbh = DBI->connect($dsn, $dbuser, $dbpass);
-	my $q = "SELECT passwd FROM auth WHERE username=\'$user\'";
-	my $QueryPW = $dbh->prepare($q);
-	$QueryPW->execute;
-	my $passwd = $QueryPW->fetchrow_array;
-	
-	$QueryPW->finish;
-	$dbh->disconnect;
 
-	my $md5 = Digest::MD5->new();
-	$md5->add("$user:idisk.mac.com:$sent_pw");
-	my $gen_passwd = $md5->hexdigest;
-	
-	return $gen_passwd;
+	my $dbh = $self->get_dbh();
+
+	#carp "Fetch Hash from DB";
+	#carp $user;
+	#carp $realm;
+	#carp $dbh;
+
+	my $QueryPW = $dbh->prepare(qq{SELECT passwd FROM auth WHERE username=? AND realm=?});
+	$QueryPW->execute($user, $realm);
+	my ($passwd) = $QueryPW->fetchrow_array;
+	$QueryPW->finish;	
+	return $passwd;
 }
 
-sub authen_user{
-	my ($dsn, $user, $sent_pw) = @_;
-	carp "AuthenDOTMAC_SQL";
-	#my $u = $user;
-	my $dbh = DBI->connect($dsn, $dbuser, $dbpass);
-	my $q = "SELECT passwd FROM auth WHERE username=\'$user\'";
-	my $QueryPW = $dbh->prepare($q);
-	$QueryPW->execute;
-	my $passwd = $QueryPW->fetchrow_array;
-	
-	$QueryPW->finish;
-	$dbh->disconnect;
+sub get_dbh {
+	shift->{dbh};
+}
 
-	my $md5 = Digest::MD5->new();
-	$md5->add("$user:idisk.mac.com:$sent_pw");
-	my $gen_passwd = $md5->hexdigest; 
+sub DESTROY {
+	my $self = shift;
+	$self->dbh->disconnect if defined $self->dbh;
+}
 
-	if ($passwd eq $gen_passwd) {
-		return 1;
+sub rearrange {
+	my($order,@param) = @_;
+	return unless @param;
+	my %param;
+
+	if (ref $param[0] eq 'HASH') {
+		%param = %{$param[0]};
 	} else {
-		return 0;
-	}
-}
+		return @param unless (defined($param[0]) && substr($param[0],0,1) eq '-');
 
-sub get_user_quota{
-	my ($dsn, $user) = @_;
-	carp "QuotaDotMac_SQL";
-	my $dbh = DBI->connect($dsn, $dbuser, $dbpass);
-	my $q = "SELECT idisk_quota_limit FROM auth WHERE username=\'$user\'"; 
-	my $dbq = $dbh->prepare($q);
-	$dbq->execute;
-	my ($quota) = $dbq->fetchrow_array;
-	$dbq->finish;
-	$dbh->disconnect;
-	return $quota;
-}
+		my $i;
+		for ($i=0;$i<@param;$i+=2) {
+			$param[$i]=~s/^\-//;     # get rid of initial - if present
+			$param[$i]=~tr/a-z/A-Z/; # parameters are upper case
+		}
 
-sub list_users{
-	my ($dsn) = @_;
-	carp "ListUsersDotMac_SQL";
-	my $dbh = DBI->connect($dsn, $dbuser, $dbpass);
-	my $q = $dbh->prepare("SELECT username FROM auth");
-	$q->execute;
-
-	my @userlist = ();
-	while (my ($user) = $q->fetchrow_array) {
-		push @userlist, $user;
+		%param = @param;                # convert into associative array
 	}
 
-	$q->finish;
-	$dbh->disconnect;
+	my(@return_array);
 
-	return sort @userlist;
+	local($^W) = 0;
+	my($key)='';
+	foreach $key (@$order) {
+		my($value);
+		if (ref($key) eq 'ARRAY') {
+			foreach (@$key) {
+				last if defined($value);
+				$value = $param{$_};
+				delete $param{$_};
+			}
+		} else {
+			$value = $param{$key};
+			delete $param{$key};
+		}
+		push(@return_array,$value);
+	}
+	push (@return_array,{%param}) if %param;
+	return @return_array;
 }
+1;
+#sub authen_user{
+#	my ($dsn, $user, $sent_pw) = @_;
+#	carp "AuthenDOTMAC_SQL";
+#	#my $u = $user;
+#	my $dbh = DBI->connect("DBI:mysql:database=dotmac;host=localhost", "dotmac", "dotmac");
+#	my $q = "SELECT passwd FROM auth WHERE username=\'$user\'";
+#	my $QueryPW = $dbh->prepare($q);
+#	$QueryPW->execute;
+#	my $passwd = $QueryPW->fetchrow_array;
+#	
+#	$QueryPW->finish;
+#	$dbh->disconnect;
+#
+#	my $md5 = Digest::MD5->new();
+#	$md5->add("$user:idisk.mac.com:$sent_pw");
+#	my $gen_passwd = $md5->hexdigest; 
+#
+#	if ($passwd eq $gen_passwd) {
+#		return 1;
+#	} else {
+#		return 0;
+#	}
+#}
+
+#sub get_user_quota{
+#	my ($dsn, $user) = @_;
+#	carp "QuotaDotMac_SQL";
+#	my $dbh = DBI->connect("DBI:mysql:database=dotmac;host=localhost", "dotmac", "dotmac");
+#	my $q = "SELECT idisk_quota_limit FROM auth WHERE username=\'$user\'"; 
+#	my $dbq = $dbh->prepare($q);
+#	$dbq->execute;
+#	my ($quota) = $dbq->fetchrow_array;
+#	$dbq->finish;
+#	$dbh->disconnect;
+#	return $quota;
+#}
+
+#sub list_users{
+#	my ($dsn) = @_;
+#	carp "ListUsersDotMac_SQL";
+#	my $dbh = DBI->connect("DBI:mysql:database=dotmac;host=localhost", "dotmac", "dotmac");
+#	my $q = $dbh->prepare("SELECT username FROM auth");
+#	$q->execute;
+#
+#	my @userlist = ();
+#	while (my ($user) = $q->fetchrow_array) {
+#		push @userlist, $user;
+#	}
+#
+#	$q->finish;
+#	$dbh->disconnect;
+#
+#	return sort @userlist;
+#}
