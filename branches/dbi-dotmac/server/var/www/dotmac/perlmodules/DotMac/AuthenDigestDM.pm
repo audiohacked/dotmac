c86f46acc6965bda75e2f0471a1a853b50698741
audiohacked <audiohacked@de5ee365-d638-0410-ac67-a579fd33fef4>
2008-01-25 18:12:17 +0000

    [add] new module to abstract use of DB Backends

--- branches/dbi-dotmac/server/var/www/dotmac/perlmodules/DotMac/AuthenDigestDM.pm
+++ branches/dbi-dotmac/server/var/www/dotmac/perlmodules/DotMac/AuthenDigestDM.pm
@@ -6,31 +6,25 @@ use strict;
 
 sub handler {
 	my ($r, $user, $realm, $hash) = @_;
+	my $dbauth;
+
 	#my $dbType = $r->dir_config('dotMacDBServType');
 	#carp $dbType;
 	my $dbType = 'mysql';
-	my $dbauth;
 
-	#carp "AuthenDigestDM";
-	#carp $dbType;
-	#carp $user;
-	#carp $realm;
-	
 	if ($dbType eq 'mysql') { # mysql
 		use DotMac::DotMacDB::MySQL;
-		$dbauth = new DotMac::DotMacDB::MySQL;
+		$dbauth = new DotMac::DotMacDB::MySQL::;
 		#carp "using mysql";
-	#} elsif ($dbType eq 'sqlite') {
-	#	use DotMac::DotMacDB::SQLite;
-	#	$dbauth = new DotMac::DotMacDB::SQLite;
-	#	carp "using sqlite";
+	} elsif ($dbType eq 'sqlite') {
+		use DotMac::DotMacDB::SQLite;
+		$dbauth = new DotMac::DotMacDB::SQLite::;
+		carp "using sqlite";
 	} else {
 		return Apache2::Const::DECLINED;
 	}
 
-	#carp "exec fetch_hash";
-	my $savedHash = $dbauth->fetch_hash($user, $realm);
-	#carp $savedHash;
+	my $savedHash = $dbauth->fetch_apache_auth($user, $realm);
 	if($savedHash) {
 		$$hash = $savedHash;
 		return Apache2::Const::OK;
