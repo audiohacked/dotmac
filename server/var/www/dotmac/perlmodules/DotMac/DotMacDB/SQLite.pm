15dcec08783489deda62efbadbabcaa9eb21b6c1
audiohacked <audiohacked@de5ee365-d638-0410-ac67-a579fd33fef4>
2008-01-25 07:40:17 +0000

    [add] SQLite support module

--- server/var/www/dotmac/perlmodules/DotMac/DotMacDB/SQLite.pm
+++ server/var/www/dotmac/perlmodules/DotMac/DotMacDB/SQLite.pm
@@ -1,80 +1,43 @@
-package DotMac::DotMacDB::MySQL;
+package DotMac::DotMacDB::SQLite;
 
 use DBI;
+use strict;
+use CGI::Carp;
 
-my $dsn = "DBI:mysql:database=dotmac;host=localhost";
-my $dbuser = 'dotmac';
-my $dbpass = 'dotmac';
+sub new {
+	my $invocant = shift;
+	my $class = ref($invocant) || $invocant;
+
+	my $dbfile = exists $_{file} ? $_{file} : "/var/www/dotmac/private/dotmac";
+
+	my $dotmacDBConn = DBI->connect("DBI:sqlite:".$dbfile, "", "");
+	my $self = {
+		dbn => $dotmacDBConn,
+
+	};
+	return bless $self, $class;
+}
 
 sub fetch_hash{
+	my $self = shift;
 	my ($user, $realm) = @_;
-	my $dbh = DBI->connect($dsn, $dbuser, $dbpass);
-	my $q = "SELECT passwd FROM auth WHERE username=\'$u\'";
-	my $QueryPW = $dbh->prepare($q);
-	$QueryPW->execute;
-	my $passwd = $QueryPW->fetchrow_array;
+	
+	my $dbh = $self->get_dbh();
+
+	my $QueryPW = $dbh->prepare(qq{SELECT passwd FROM auth WHERE username=? AND realm=?});
+	$QueryPW->execute($user, $realm);
+	my ($passwd) = $QueryPW->fetchrow_array;
 	
 	$QueryPW->finish;
-	$dbh->disconnect;
-
-	my $md5 = Digest::MD5->new();
-	$md5->add("$u:idisk.mac.com:$sent_pw");
-	my $gen_passwd = $md5->hexdigest;
-	
-	return $gen_passwd;
+	return $passwd;
 }
 
-sub authen_user{
-	my ($dsn, $user, $sent_pw) = @_;
-	carp "AuthenDOTMAC_SQL";
-	my $u = $user;
-	my $dbh = DBI->connect($dsn, $dbuser, $dbpass);
-	my $q = "SELECT passwd FROM auth WHERE username=\'$u\'";
-	my $QueryPW = $dbh->prepare($q);
-	$QueryPW->execute;
-	my $passwd = $QueryPW->fetchrow_array;
-	
-	$QueryPW->finish;
-	$dbh->disconnect;
-
-	my $md5 = Digest::MD5->new();
-	$md5->add("$u:idisk.mac.com:$sent_pw");
-	my $gen_passwd = $md5->hexdigest; 
-
-	if ($passwd eq $gen_passwd) {
-		return 1;
-	} else {
-		return 0;
-	}
+sub get_dbh {
+	shift->{dbh};
 }
 
-sub get_user_quota{
-	my ($dsn, $user) = @_;
-	carp "QuotaDotMac_SQL";
-	my $dbh = DBI->connect($dsn, $dbuser, $dbpass);
-	my $q = "SELECT idisk_quota_limit FROM auth WHERE username=\'$user\'"; 
-	my $dbq = $dbh->prepare($q);
-	$dbq->execute;
-	my ($quota) = $dbq->fetchrow_array;
-	$dbq->finish;
-	$dbh->disconnect;
-	return $quota;
+sub DESTROY {
+	my $self = shift;
+	$self->dbh->disconnect if defined $self->dbh;
 }
-
-sub list_users{
-	my ($dsn) = @_;
-	carp "ListUsersDotMac_SQL";
-	my $dbh = DBI->connect($dsn, $dbuser, $dbpass);
-	my $q = $dbh->prepare("SELECT username FROM auth");
-	$q->execute;
-
-	my @userlist = ();
-	while (my ($user) = $q->fetchrow_array) {
-		push @userlist, $user;
-	}
-
-	$q->finish;
-	$dbh->disconnect;
-
-	return sort @userlist;
-}
+1;
