9cdcb465f881fcf5799dab64f423c0d642dbd9b1
audiohacked <audiohacked@de5ee365-d638-0410-ac67-a579fd33fef4>
2008-01-26 07:50:24 +0000

    [testing] various work on DotMacDB Interface

--- branches/dbi-dotmac/server/var/www/dotmac/perlmodules/DotMac/AuthenDigestDM.pm
+++ branches/dbi-dotmac/server/var/www/dotmac/perlmodules/DotMac/AuthenDigestDM.pm
@@ -1,20 +1,29 @@
 package DotMac::AuthenDigestDM;
 
+use DotMac::DotMacDB;
 use Apache2::Const -compile => qw(OK DECLINED HTTP_UNAUTHORIZED);
 use CGI::Carp;
 use strict;
 
 sub handler {
+	carp "AuthenDigestDM";
 	my ($r, $user, $realm, $hash) = @_;
 	my $dbauth;
 
-	#my $dbType = $r->dir_config('dotMacDBServType');
-	#carp $dbType;
-	my $dbType = 'mysql';
+	my $dbType = $r->dir_config('dotMacDBServType');
+	carp $dbType;
+	carp $user;
+	carp $realm;
 
-	$dbauth = DotMac::DotMacDB::new(provider => $dbType);
+	$dbauth = DotMac::DotMacDB->new( -provider=>'mysql', 
+		-db=>'dotmac',
+		-host=>'localhost',
+		-user=>'dotmac',
+		-pass=>'dotmac'
+		);
 
 	my $savedHash = $dbauth->fetch_apache_auth($user, $realm);
+	carp $savedHash;
 	if($savedHash) {
 		$$hash = $savedHash;
 		return Apache2::Const::OK;
--- branches/dbi-dotmac/server/var/www/dotmac/perlmodules/DotMac/DotMacDB.pm
+++ branches/dbi-dotmac/server/var/www/dotmac/perlmodules/DotMac/DotMacDB.pm
@@ -2,48 +2,106 @@ package DotMac::DotMacDB;
 
 use strict;
 use CGI::Carp;
+use Data::Dumper;
+#use DotMac::Utils::Rearrange;
+use DotMac::DotMacDB::mysql;
 
 sub new {
-	my ($self, @args) = @_;
-	my $db_provider = exists $args{provider} ? $args{provider} : "mysql";
+	carp "new DotMacDB";
+	my $self = shift;
+
+	my ($db_provider, @args) = rearrange(['provider'], @_);
+	$db_provider ||= 'mysql';
+
 
 	my $backend = "DotMac::DotMacDB::\L${db_provider}\E";
-	eval "require $backend" unless $backend->can('new');
+	eval "use $backend" if $backend->can('new');
+
+	#my $backend = "DotMac::DotMacDB::mysql";
 
 	my $this = bless {}, $self;
-	$this->{backend} = $class->new(@args);
+	$this->{backend} = $backend->new(@args);
 	return $this;
 }
 
 sub fetch_apache_auth{
+	carp "DotMacDB: fetch_apache_auth";
 	my $self = shift;
 	my ($user, $realm) = @_;
 
+
 	$realm ||= 'idisk.mac.com';
-	return $self->{backend}->fetch_apache_auth($user, $realm);
+	return $self->{backend}->fetch_apache_auth($user, $realm); # if $self->{backend}->can('fetch_apache_auth');
 }
 
 sub authen_user{
+	carp "DotMacDB: authen_user";
 	my $self = shift;
 	my ($user, $sent_pw, $realm) = @_;
 
 	$realm ||= 'idisk.mac.com';
-	return $self->{backend}->authen_user($user, $sent_pw, $realm);
+	return $self->{backend}->authen_user($user, $sent_pw, $realm);# if $self->{backend}->can('authen_user');
 }
 
 sub get_user_quota{
 	my $self = shift;
 	my ($user, $realm) = @_;
 
+	carp "DotMacDB: get_user_quota";
+
 	$realm ||= 'idisk.mac.com';
-	return $self->{backend}->get_user_quota($user, $realm);
+	return $self->{backend}->get_user_quota($user, $realm);# if $self->{backend}->can('get_user_quota');
 }
 
 sub list_users{
 	my $self = shift;
 	my ($realm) = @_;
 
-	$realm ||= 'idisk.mac.com';
+	carp "DotMacDB: list_users";
 
-	return $self->{backend}->list_users($realm);
+	$realm ||= 'idisk.mac.com';
+	return $self->{backend}->list_users($realm);# if $self->{backend}->can('list_users');
 }
+
+sub rearrange {
+    my($order,@param) = @_;
+    return unless @param;
+    my %param;
+
+    if (ref $param[0] eq 'HASH') {
+      %param = %{$param[0]};
+    } else {
+      return @param unless (defined($param[0]) && substr($param[0],0,1) eq '-');
+
+      my $i;
+      for ($i=0;$i<@param;$i+=2) {
+        $param[$i]=~s/^\-//;     # get rid of initial - if present
+        $param[$i]=~tr/a-z/A-Z/; # parameters are upper case
+      }
+
+      %param = @param;                # convert into associative array
+    }
+
+    my(@return_array);
+
+    local($^W) = 0;
+    my($key)='';
+    foreach $key (@$order) {
+        my($value);
+        if (ref($key) eq 'ARRAY') {
+            foreach (@$key) {
+                last if defined($value);
+                $value = $param{$_};
+                delete $param{$_};
+            }
+        } else {
+            $value = $param{$key};
+            delete $param{$key};
+        }
+        push(@return_array,$value);
+    }
+    push (@return_array,{%param}) if %param;
+    return @return_array;
+}
+
+1;
