60eb4440c310d360cbdce4d0b2c6b74d2c6bfdc2
rob.see <rob.see@de5ee365-d638-0410-ac67-a579fd33fef4>
2008-11-16 15:56:59 +0000

    Added check to make sure that blank usernames don't get inserted (which causes apache to choke on startup)

--- branches/iLife/server/var/www/dotmac/private/genAdminAccount.pl
+++ branches/iLife/server/var/www/dotmac/private/genAdminAccount.pl
@@ -5,9 +5,10 @@ use DBI;
 
 my $connectstr="dbi:SQLite:./dotmac";
 my $dbh=DBI->connect($connectstr);
-my $user=$ARGV[0];
-my $newpass=$ARGV[1];
+my $user=$ARGV[0]||die "You must provide a username as the first argument\n";
+my $newpass=$ARGV[1]||die "You must provide a password as the second argument\n";
 my $realm = "idisk.mac.com";
+
 
 my $md5 = Digest::MD5->new();
 $md5->add("$user:$realm:$newpass");
