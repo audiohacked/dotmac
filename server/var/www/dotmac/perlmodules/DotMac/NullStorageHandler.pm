ec6fcf464191e3ba9a36648b254505c58ae3274b
walinsky <walinsky@de5ee365-d638-0410-ac67-a579fd33fef4>
2009-05-05 22:17:28 +0000

    [Fix] the dotmac preferences pane would reject passwords with characters outside [a-zA-Z]

--- server/var/www/dotmac/perlmodules/DotMac/NullStorageHandler.pm
+++ server/var/www/dotmac/perlmodules/DotMac/NullStorageHandler.pm
@@ -1,3 +1,8 @@
+#we shouldn't need this
+#http://perl.apache.org/docs/2.0/api/Apache2/RequestRec.html#C_filename_
+
+#upon successfully proxying we can update the file's stat record like:
+#$r->finfo(APR::Finfo::stat($filename, APR::Const::FINFO_NORM, $r->pool));
 package DotMac::NullStorageHandler;
   
   use strict;
