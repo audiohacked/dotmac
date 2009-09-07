6c3619d5b542751252247100723149c43a8784e5
rob.see <rob.see@de5ee365-d638-0410-ac67-a579fd33fef4>
2009-09-07 03:56:20 +0000

    more setup

---
+++ setup/06_prepare_gallery.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+
+
+use Cwd;
+use File::Copy;
+use Data::Dumper;
+use strict;
+
+
+sub readConf {
+		my $filetoread;
+		my $line;
+	if (-f "/etc/dotmobile.us/conf" ) {
+		$filetoread="/etc/dotmobile.us/conf";
+		print "Reading Config file from $filetoread\n";
+	}  else {
+		print "No config file found, exiting";
+		exit(1);
+	}
+	open(CONF,"<$filetoread");
+	my @tmparr;
+	my $varhash={};
+	while ($line=<CONF>){
+		chomp($line);
+		if ($line =~ /.*=.*/) {
+			@tmparr=split(/=/,$line);
+			$varhash->{@tmparr[0]}=@tmparr[1];
+		}
+	}
+	return $varhash;
+}
+
+
+my $conf=readConf();
+my $cachedir=$conf->{'DOTMOBILEROOT'}."/cache/";
+my $localwwwmacname=$conf->{'LOCALWWWMACNAME'};
+my $localidiskname=$conf->{'LOCALIDISKNAME'};
+my $localpublishname=$conf->{'LOCALPUBLISHNAME'};
+my $localgalleryname=$conf->{'LOCALGALLERYNAME'};
+my $filetouse="gallery.ht";
+my $fullpath=$cachedir."/".$filetouse;
+my	$uri="http://gallery.mac.com/emily_parker";
+	print "Downloading index page from $uri \n";
+#	`curl $uri > $fullpath`;
+
+print "Splitting $localgalleryname into parts\n";
+my $hostname;
+my $domain;
+if ($localgalleryname =~ /^([^.]*)\.(.*)/) {
+	$hostname=$1;
+	$domain=$2;	
+}
+
+print "Rewriting Domain\n";
+`perl -p -i -e "s/^var currentDomain.*\;/var currentDomain = \'$domain\'\;/" $fullpath`;
+
+print "Rewriting Hostname\n";
+`perl -p -i -e "s/^var hostName.*\;/var hostName = \'$localgalleryname\'\;/" $fullpath`;
+
+$uri="http://gallery.mac.com/g/javascripts/gallery.js?1249509518";
+$filetouse="gallery.js";
+$fullpath=$cachedir."/g/javascripts/".$filetouse;
+
+print $fullpath."\n";
+print "Downloading gallery page from $uri \n";
+	`curl $uri > $fullpath`;
+
+print "Rewriting Domain\n";
+`perl -p -i -e "s/var DOMAIN=.*?\;/var DOMAIN = \'$domain\'\;/" $fullpath`;
+
+print "Rewriting Hostname\n";
+`perl -p -i -e "s/var HOST=.*?\;/var HOST= \'$localgalleryname\'\;/" $fullpath`;
+
+print "Removing Annoying .com reference\n";
+`perl -p -i -e 's|\\\\.com||g' $fullpath`;
+
+#print "Replacing publish.me.com with local name\n";
+#`find $configurationdir -name '*.plist' -exec perl -p -i -e \"s/publish.me.com/$localpublishname/g\" {} \\;`;
