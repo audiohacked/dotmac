01f49a158364356650aa4862f38bc82a61f7cbfb
rob.see <rob.see@de5ee365-d638-0410-ac67-a579fd33fef4>
2009-09-08 22:28:37 +0000

    Added custom DNS names to the server certificate
    Resolves: DOTMOBILE-9

--- setup/09_sign_server_cert.pl
+++ setup/09_sign_server_cert.pl
@@ -41,7 +41,15 @@ my $SSLEAY_CONFIG="$certsdir/dotmacssl.cnf";
 my $CA="$openssl ca -config $SSLEAY_CONFIG";
 my $REQ="$openssl req -config $SSLEAY_CONFIG";
 
+my $idisksrv=$conf->{'LOCALIDISKNAME'};
+my $wwwmacname=$conf->{'LOCALWWWMACNAME'};
+
+my $dnsaltline="subjectAltName = DNS:www.mac.com, DNS:idisk.mac.com, DNS:syncmgmt.mac.com, DNS:lcs.mac.com, DNS:certinfo.mac.com, DNS:configuration.apple.com, DNS:m3.mac.com, DNS:$wwwmacname, DNS:$idisksrv\n";
+open (EXTN,">$root/private/extensions");
+print EXTN $dnsaltline;
+close(EXTN);
 `$REQ -new -nodes -keyout $root/private/server.key -out $root/private/server.csr -subj "/C=DM/ST=SomeState/O=dotMobile.us/CN=dot.mac.com"`;
 
-`$CA -days 365 -in $root/private/server.csr -extfile $root/private/extensions -out $root/private/server.crt`;
+`$CA -days 365 -in $root/private/server.csr -extfile $root/private/extensions -out $root/private/server.crt -batch`;
 
+print "Server certificates generated. Apache will use then next time you reboot\n";
