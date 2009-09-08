43fc448ca479cd7a94151a72454614a36ab2dd0f
rob.see <rob.see@de5ee365-d638-0410-ac67-a579fd33fef4>
2009-09-08 00:56:38 +0000

    Remove unneeded dependencies

--- branches/robsworking/server/var/www/dotmac/perlmodules/DotMobileAdmin/main.pm
+++ branches/robsworking/server/var/www/dotmac/perlmodules/DotMobileAdmin/main.pm
@@ -11,8 +11,8 @@ use Apache2::RequestRec ();
 use Apache2::RequestIO ();
 
 use Apache2::Const -compile => qw(OK HTTP_PAYMENT_REQUIRED);
-use CGI::FastTemplate;
-use HTML::Template;
+#use CGI::FastTemplate;
+#use HTML::Template;
 use Embperl;
 $DotMobileAdmin::locate::VERSION = '0.1';
 
